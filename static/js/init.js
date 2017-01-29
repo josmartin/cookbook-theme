53c2bd2f60a78b65dec2ad30de799d0d5b3b8ed5
Jos Martin <jos.martin@gmail.com>
2017-01-29 18:05:04 +0000

    Initial theme checkin

---
+++ static/js/init.js
@@ -0,0 +1,21 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav({
+		menuWidth: 250,
+		closeOnClick: false});
+		
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+(function() {
+    var cx = '010224182914260759138:id98l8rtr5a';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+})();  
